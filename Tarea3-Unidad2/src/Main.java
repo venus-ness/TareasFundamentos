25edf83721ddca7642a64e205c8216c4df0fc33e
venus-ness <nessa.gonzalezcalderon@gmail.com>
2024-09-11 23:13:53 -0600

    Tarea 3 terminada

---
+++ Tarea3-Unidad2/src/Main.java
@@ -0,0 +1,51 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Sección 1: Información de un libro
+
+        System.out.println("Los demonios del eden");
+        System.out.println("Lydia Cacho");
+        System.out.println("2006");
+        System.out.println("Feminismo/Politica");
+        System.out.println("272 paginas");
+
+        // Sección 2: Información de un país que deseo visitar
+
+        String NombreDelPais = "Japon";
+
+        String Capital = "Tokyo";
+
+        String IdiomaOficial = "Japones";
+
+        int Poblacion = 126967826;
+
+        String moneda = "Yen Japones";
+
+        System.out.println("Nombre del pais: " + NombreDelPais);
+        System.out.println("Capital del pais: " + Capital);
+        System.out.println("Idioma del pais: " + IdiomaOficial);
+        System.out.println("Poblacion: " + Poblacion);
+        System.out.println("moneda: " + moneda);
+
+        // Sección 3: Información de un personaje historico favorito
+
+        String NombreCompleto = "Cleopatra VII Thea Filopator";
+
+        String FechaDeNacimiento = "69 a.C";
+
+        String Nacionalidad = "Macedonio-griego";
+
+        String Ocupacion = "Reino Egipto";
+
+        String LogroDestacado = "Apoyo y fomento a las artes";
+
+        System.out.println("Nombre Completo =" + NombreCompleto);
+        System.out.println("Fecha de Nacimiento =" + FechaDeNacimiento);
+        System.out.println("Nacionalidad =" + Nacionalidad);
+        System.out.println("Ocupacion =" + Ocupacion);
+        System.out.println("LogroDestacado =" + LogroDestacado);
+
+        }
+    }
